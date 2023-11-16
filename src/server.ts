d62f5272ff7d019146a8436461a95916a3626874
ajaykmr8684 <ajaykmr8684@gmail.com>
2023-11-16 12:36:38 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
+    throw new Error('Something went wrong');
     app.listen(PORT, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Listening on port ${PORT}`);
+      logger.info(`Listening on port ${PORT}`);
     });
   } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-    process.exit(1);
+    if (err instanceof Error) {
+      logger.error(err.message);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
   }
 };
 
