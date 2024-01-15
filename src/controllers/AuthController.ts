029d14c04a4dca949e0464fbddd435fe9f8b6f0d
Ajay Kumar <ajaykmr8684@gmail.com>
2024-01-16 00:24:41 +0530

    add test for persist user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from 'express';
-
-interface CreatePost {
-  id: number;
-  title: string;
-  content: string;
-}
+import { Response } from 'express';
+import { RegisterUserRequest } from '../types';
+import { UserService } from '../services/UserService';
 
 export class AuthController {
-  id: number = 1;
-  posts: CreatePost[] = [];
-  register(req: Request, res: Response) {
+  userService: UserService;
+
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  async register(req: RegisterUserRequest, res: Response) {
+    const { firstName, lastName, email, password } = req.body;
+    await this.userService.create({ firstName, lastName, email, password });
     res.status(200).json();
   }
 }
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,28 @@
 import request from 'supertest';
 import app from '../../src/app';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../src/config/data-source';
+import { truncateTables } from '../utils';
+import { User } from '../../src/entity/User';
 
 describe('POST /auth/register', () => {
+  let connection: DataSource;
+
+  //Initialising the DB first before any test
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+  });
+
+  beforeEach(async () => {
+    //Database Truncate before each test
+    await truncateTables(connection);
+  });
+
+  //Closing the DB after the Suite ends
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
   describe('Given all fields', () => {
     it('should return 200 OK status code', async () => {
       //Arrange
@@ -9,6 +30,7 @@ describe('POST /auth/register', () => {
         firstName: 'Ajay',
         lastName: 'Kumar',
         email: 'informalajay@gmail.com',
+        password: 'secret',
       };
       //Act
       const response = await request(app).post('/auth/register').send(userData);
@@ -22,6 +44,7 @@ describe('POST /auth/register', () => {
         firstName: 'Ajay',
         lastName: 'Kumar',
         email: 'informalajay@gmail.com',
+        password: 'secret',
       };
       //Act
       const response = await request(app).post('/auth/register').send(userData);
@@ -32,15 +55,20 @@ describe('POST /auth/register', () => {
     });
 
     it('should persist the user in the database', async () => {
-      //Arrange
-      // const userData = {
-      //     firstName: "Ajay",
-      //     lastName: "Kumar",
-      //     email: "informalajay@gmail.com"
-      // }
-      //Act
-      // const response = await request(app).post("/auth/register").send(userData);
-      //Assert
+      // Arrange
+      const userData = {
+        firstName: 'Ajay',
+        lastName: 'Kumar',
+        email: 'informalajay@gmail.com',
+        password: 'secret',
+      };
+      // Act
+      await request(app).post('/auth/register').send(userData);
+
+      // Assert
+      const userRespository = connection.getRepository(User);
+      const users = await userRespository.find();
+      expect(users).toHaveLength(1);
     });
   });
 
