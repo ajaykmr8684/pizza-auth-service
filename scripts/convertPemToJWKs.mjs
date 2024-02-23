40b86dddde7fb5fea422ed535b1150fbc74863bf
Ajay Kumar <ajaykmr8684@gmail.com>
2024-02-23 16:14:37 +0530

    refactor pemToJwk script

--- scripts/convertPemToJWKs.mjs
+++ scripts/convertPemToJWKs.mjs
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import rsaPemToJwk from "rsa-pem-to-jwk"
-import fs from "fs"
+import rsaPemToJwk from 'rsa-pem-to-jwk';
+import fs from 'fs';
 
-const privateKey = fs.readFileSync("./certs/private.pem");
+const privateKey = fs.readFileSync('./certs/private.pem');
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const jwk = rsaPemToJwk(privateKey, {use: "sig"}, "public");
+const jwk = rsaPemToJwk(privateKey, { use: 'sig' }, 'public');
 
 console.log(JSON.stringify(jwk));
